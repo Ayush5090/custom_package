a48af9f3fc641970c469c3cdfcf7f316c735d12e
unknown <ayushm1151@gmail.com>
2024-09-23 14:47:20 +0530

    Custom Elevated Button

--- lib/custom_package.dart
+++ lib/custom_package.dart
@@ -1,7 +1,35 @@
 library custom_package;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+export 'custom_package.dart';
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final Widget child;
+  final Color? bgColor;
+  final double? borderRadius;
+  final double? elevation;
+  final Size? buttonSize;
+
+  const CustomButton(
+      {super.key,
+      required this.onTap,
+      required this.child,
+      this.bgColor,
+      this.borderRadius,
+      this.elevation,
+      this.buttonSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+            backgroundColor: bgColor,
+            elevation: elevation,
+            fixedSize: buttonSize,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(borderRadius ?? 15))),
+        child: child);
+  }
 }
